4cbb1110fab207c52ac10cd91888bd385db0750e
Florian.Lindinger <florianlindinger26@gmail.com>
2025-07-23 00:30:54 +0200

    moved yaml settings import in pyhton to python helper file settings.py. Changed comments;renamed file; update todo list

--- code/main_code.py
+++ code/main_code.py
@@ -1,22 +1,9 @@
-######################################################################################################################################################
+####################################################################################################################################
 # Add code at the bottom that runs with the start of the program.
-######################################################################################################################################################
-# Import the user defined variables (given in settings.yaml)
-# with the code lines below (e.g. "name: variable" in settings.yaml
-# can be accessed here with s["name"])
-######################################################################################################################################################
-# autopep8: off
-import yaml  # install as pyyaml                                      
-import re                                                             
-with open("settings.yaml") as file: s = yaml.safe_load(file)  # get values with s["name"]	          
-# Fix what yaml can't interpret: Converts scientific notaion (as exepted in python) and math operation (+-*/^) of 2 float convertables (including scientific notaion) to float:
-float_regex = r"\s*[-+]?(?:\d+\.?\d*|\.\d+)(?:[eE][-+]?\d+)?\s*"  # identifies strings that python recognises as float convertable 
-def is_match(x, symbol): return (isinstance(x, str) and bool(re.match(f"^{float_regex}[{symbol}]{float_regex}$", x)))  # test if string & if math operation between 2 float convertables 
-s = {key: float(val) if (isinstance(val, str) and bool(re.match(f"^{float_regex}$", val))) else val for key, val in s.items()} 
-s = {key: float(val.split(r"/")[0])/float(val.split(r"/")[1]) if is_match(val, r"/") else val for key, val in s.items()}     #type:ignore
-s = {key: float(val.split("^")[0])**float(val.split("^")[1]) if is_match(val, r"\^") else val for key, val in s.items()}     #type:ignore
-s = {key: float(val.split("*")[0])*float(val.split("*")[1]) if is_match(val, "*") else val for key, val in s.items()}        #type:ignore
-s = {key: float(val.split("+")[0])+float(val.split("+")[1]) if is_match(val, "+") else val for key, val in s.items()}        #type:ignore
-s = {key: float(val.split("-")[0])-float(val.split("-")[1]) if is_match(val, "-") else val for key, val in s.items()}        #type:ignore
-# autopep8: on
-######################################################################################################################################################
+####################################################################################################################################
+import settings # imports and converts user variables (e.g., name: value) in settings.yaml (access value via dictionary: s["name"])
+####################################################################################################################################
+
+
+
+
---
+++ code/settings.py
@@ -0,0 +1,57 @@
+# Helper code used to import settings in settings.yaml as the dictionary s (e.g., value from the line "name: value"
+# in settings.yaml can be accessed as s["name"]).
+# Yaml automatically interprets the data types, with vales that it can't interpret otherwise becoming strings.
+# This code also converts string vales to float that yaml does not manage to interpret like some python acceptable
+# scientific notation and some simple math operations between float convertables (including scientific notation).
+
+# Implemented math operations are:
+# a+b
+# a-b
+# a*b
+# a/b
+# a^b
+# a*b^c
+# a/b^c
+
+import re
+import yaml  # install as pyyaml
+
+# import yaml file
+with open("settings.yaml") as file:
+    s = yaml.safe_load(file)  # get values with s["name"]
+
+# identifies strings that python recognises as normal float convertable:
+float_regex = r"\s*[-+]?(?:\d+\.?\d*|\.\d+)(?:[eE][-+]?\d+)?\s*"
+
+
+# tests if string & if math operation between 2 float convertables:
+def is_match(x, symbol):
+    return (isinstance(x, str) and
+            bool(re.match(f"^{float_regex}[{symbol}]{float_regex}$", x)))
+
+
+# tests if string & if math operation between 3 float convertables:
+def is_match2(x, symbol1, symbol2):
+    return (isinstance(x, str) and
+            bool(re.match(f"^{float_regex}[{symbol1}]{float_regex}[{symbol2}]{float_regex}$", x)))
+
+
+# Fix some of what yaml can't interpret: Converts scientific notation (as accepted in python) & implemented math operations (+-*/^) of 2/3 float convertables (including scientific notation) to float:
+s = {key: float(val) if (isinstance(val, str) and bool(re.match(f"^{float_regex}$", val)))
+     else val for key, val in s.items()}
+s = {key: float(val.split("+")[0]) + float(val.split("+")[1]) if  # type:ignore
+     is_match(val, "+") else val for key, val in s.items()}
+s = {key: float(val.split("-")[0]) - float(val.split("-")[1]) if   # type:ignore
+     is_match(val, "-") else val for key, val in s.items()}
+s = {key: float(val.split("*")[0]) * float(val.split("*")[1]) if  # type:ignore
+     is_match(val, "*") else val for key, val in s.items()}
+s = {key: float(val.split(r"/")[0]) / float(val.split(r"/")[1]) if  # type:ignore
+     is_match(val, r"/") else val for key, val in s.items()}
+s = {key: float(val.split("^")[0]) ** float(val.split("^")[1]) if  # type:ignore
+     is_match(val, r"\^") else val for key, val in s.items()}
+s = {key: float(val.split("*")[0]) * float(val.split("*")[1].split("^")[0]) **  # type:ignore
+     float(val.split("*")[1].split("^")[1]) if is_match2(val, "*", r"\^")       # type:ignore
+     else val for key, val in s.items()}
+s = {key: float(val.split(r"/")[0]) / float(val.split(r"/")[1].split("^")[0]) **  # type:ignore
+     float(val.split(r"/")[1].split("^")[1]) if is_match2(val, r"/", r"\^")       # type:ignore
+     else val for key, val in s.items()}
