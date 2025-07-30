3d09d52ef000f7bed488561bafc43df3f5f2653a
Florian.Lindinger <florianlindinger26@gmail.com>
2025-07-31 00:22:24 +0200

    renamed generator batch; made python file to default again

--- code/main_code.py
+++ code/main_code.py
@@ -3,13 +3,3 @@
 ####################################################################################################################################
 import settings # imports and converts user variables (e.g., name: value) in settings.yaml (access value via dictionary: s["name"])
 ####################################################################################################################################
-
-# import time
-# time.sleep(10)
-
-import time
-for i in range(1000):
-    print(i)
-    time.sleep(0.01)
-
-
